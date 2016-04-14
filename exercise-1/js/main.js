e7842280ee72ea35f9f3337c7e604f6af6b9c82c
C Nicholas Mao <mao@tacktechnologies.com>
2016-04-13 18:47:11 -0700

    omg people are on the screen

--- exercise-1/js/main.js
+++ exercise-1/js/main.js
@@ -4,13 +4,23 @@
 $(function() {
 
   // Select the element with id `sandbox` and append a `div` element to it. Make sure to store the div in a JavaScript varaible
+  var div = d3.select("#sandbox").append("div");
 
   // Create a variable `mySvg` by appending an `svg` element to your newly created `div`
+  var mySvg = div.append("mySvg");
 
   // Set both the width and height attributes of your `svg` to 300 
+  mySvg.attr('width', 300)
+  		.attr('height', 300);
+
 
   // Append a `circle` element to your `svg`, setting the properties noted in index.html
+  mySvg.append('circle')
+  		.attr('r', 30)
+  		.attr('cx', 75)
+  		.attr('cy', 30)
+  		.style('opacity', 0.5);
 
   // Append a `rect` element to your `svg`, setting the properties noted in index.html
-
+  
 });
--- exercise-2/js/main.js
+++ exercise-2/js/main.js
@@ -16,14 +16,23 @@ var people2 = [
 $(function() {
   // Create a varaible myDiv by selecting the element with id my-div (this is already created for you).
 
+  var myDiv = d3.select("#my-div");
+
+  var paragraphs = myDiv.selectAll("p").data(people, function(d) {return d.name} );
 
   // Create a variable `paragraphs` by selecting all  `p` elements in  `myDiv`, and binding you data  `people` to it.
   // Make sure to specify the `name` attribute as an identifying characteristic in your data-join.
 
-
-
   // Add a paragraph to the div for each person in your dataset following instructions in the index.html file
 
+  paragraphs.enter()
+    .append('p')
+    .text(function(d) {
+      return d.name
+    })
+    .style('color', function(d) {
+      return d.favColor
+    });
 
   // Use `setTimeout` function to wait 3 seconds, then do the following
 
